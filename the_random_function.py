f39eba33b89b477b7df4cfc3572157df3389bbe1
teammcarthur <chrism.23092@obhs.school.nz>
2026-08-14 13:15:19 +1200

    commenting

--- the_random_function.py
+++ the_random_function.py
@@ -11,7 +11,7 @@ import random
 
 #-----Main Routine------
 if(__name__ == "__main__"):
-    contestant = input("Please enter your name")
-    answer = random.randint(1, 10)
-    guess = int(0)
+    contestant = input("Please enter your name") #Gets the user to enter their name and stores it 
+    answer = random.randint(1, 10) #Sets the answer to a random number between 1 and 10
+    guess = int(0) #initialises the guess as an int
     count = 0
