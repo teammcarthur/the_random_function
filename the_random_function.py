a06b482584df762a09679fffa4056b7b1903a60d
teammcarthur <chrism.23092@obhs.school.nz>
2026-08-14 13:22:29 +1200

    Entered the default loop to be fixed

--- the_random_function.py
+++ the_random_function.py
@@ -15,3 +15,11 @@ if(__name__ == "__main__"):
     answer = random.randint(1, 10) #Sets the answer to a random number between 1 and 10
     guess = int(0) #initialises the guess as an int
     count = 0
+
+    #loop the user guessing until correct
+    while guess != answer:
+        guess = int(input("Please enter a number from 1-10"))
+        if guess > answer: print("Your guess is too high")
+        if guess < answer: print("Your guess is too low")
+        count = count+1
+    print("Well done", contestant, "You got it right, the answer was", answer, "it took you", count, "guesses")
