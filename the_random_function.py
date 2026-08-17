f277328cf05900ddccb99aedbc1350ffdce84688
teammcarthur <chrism.23092@obhs.school.nz>
2026-08-17 14:47:16 +1200

    comment and updated code to meet requirements

--- the_random_function.py
+++ the_random_function.py
@@ -12,14 +12,14 @@ import random
 #-----Main Routine------
 if(__name__ == "__main__"):
     contestant = input("Please enter your name") #Gets the user to enter their name and stores it 
-    answer = random.randint(1, 10) #Sets the answer to a random number between 1 and 10
+    answer = random.randint(1, 20) #Sets the answer to a random number between 1 and 10
     guess = int(0) #initialises the guess as an int
     count = 0
 
     #loop the user guessing until correct
     while guess != answer:
-        guess = int(input("Please enter a number from 1-10"))
+        guess = int(input("Please enter a number from 1-20"))
         if guess > answer: print("Your guess is too high")
         if guess < answer: print("Your guess is too low")
-        count = count+1
+        count = count+1 #increases the count by 1 each loop
     print("Well done", contestant, "You got it right, the answer was", answer, "it took you", count, "guesses")
